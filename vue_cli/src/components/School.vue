683c53c7503328bd0aad3eeef2d62bbf73f07c26
wecreate <2633132826@qq.com>
2022-11-02 18:55:59 +0800

    提交 Vue 脚手架学习记录

---
+++ vue_cli/src/components/School.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 组件的结构 -->
+  <div class="demo">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+    <button @click="showName">点我提示校名</button>
+  </div>
+</template>
+
+<script>
+// 组件的交互
+export default {
+  name: 'AppSchool',
+  data() {
+    return {
+      name: 'wecreate',
+      address: '西安',
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.name);
+    },
+  },
+};
+</script>
+
+<style>
+/* 组件的样式 */
+.demo {
+  background-color: rgb(34, 152, 255);
+}
+</style>
